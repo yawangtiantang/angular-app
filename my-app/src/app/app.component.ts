484374502e7c32fd57488047d22dd2d3c860f0b8
zhouxu <yawang1995@163.com>
2019-06-18 15:47:31 +0800

    双向数据绑定

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+template:
+`<input type='text' [(ngModel)]='msg'>
+<h1>{{msg}}</h1>
+
+`,
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
   title = 'my-app';
+  msg = '这是输入框的内容';
 }
